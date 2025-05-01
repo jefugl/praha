c5be6be4b224b6fe506ceeb66b764a92d76f14fc
jefugl <107198804+jefugl@users.noreply.github.com>
2025-05-01 18:21:44 +0200

    push to github

---
+++ inputdata.py
@@ -0,0 +1,22 @@
+latitude = [50.08173148751279, 50.083417781455246, 50.08421538198598, 50.086049224120984, 50.08720835371793,
+            50.08875090834032, 50.090119941905165, 50.091413201550836]
+longitude = [14.426576497417004, 14.423308511067663, 14.421341106646786, 14.417997653396435, 14.408429465312867,
+             14.402061336478564, 14.403469318632586, 14.401557488978911]
+
+pubname = ['1.EA Hotel Rokoko', '2.Pinkasů', '3.McCarthy\'s Irish Pub', '4.Zlatého Tygra', '5.Café Márnice',
+           '6.U Kocoura', '7.Hrocha', '8.Mål']
+pubaddress = ["Václavské nám. 794/38, 110 00 Nové Město, Tsjekkia", "Jungmannovo nám. 756 /16, 110 00 Můstek, Tsjekkia",
+              "Rytířská 406/10, 110 00 Staré Město, Tsjekkia", "Husova 228/17 Staré Město Praha 1 Praha 1, 110 00 Praha 1, Tsjekkia",
+              "U Lužického semináře, Malá, Hroznová 489/3, Malá Strana, 118 00 Praha, Tsjekkia", "2, Nerudova 205, Malá Strana, 118 00 Praha, Tsjekkia",
+              "Thunovská 10, 118 00 Malá Strana, Tsjekkia", "Hradčany, 119 08 Prague 1, Tsjekkia"]
+
+Rokoko = 'https://lh3.googleusercontent.com/gps-proxy/ALd4DhEyTua_Tm9XmuEv7oNv-8UC2aA_DpuLqOfj4elyJDCTArzk1Z0CowHiu6gZ3QU96GWYiofYbD3i0t1tYXbIEaGn2N60PvpHCP17g7gMcEvELihGK-yrXz7Ehvj0HRcX9xVeXablj5ORh4_dWY8mFcmi6OAlOXze2b3MeB3niWx5SBuaoy2BB1yU9Q=w408-h271-k-no'
+Pinkasů = 'https://lh3.googleusercontent.com/p/AF1QipNcPAwN4x1K01hsbUpGQNmh2-3udnY-cDcHgjU7=w408-h272-k-no'
+Irish = 'https://lh3.googleusercontent.com/p/AF1QipOPR063GzTQN4u2crzSBehB3qXjlEzwIdv8tC_C=w408-h272-k-no'
+Tygra = 'https://lh3.googleusercontent.com/p/AF1QipNfXe0nwTU-nxOawws7wXJTR5wW9vKIzxtKHNaT=w408-h279-k-no'
+Márnice = 'https://lh3.googleusercontent.com/gps-cs-s/AC9h4nr1y5GsZL_xSf47dIAlgBUvwklx1-TL5Vsg5Jzin49UNrpktzGI0JzOu1JPHPPff8kZIYAAtUplAVyk2bXf_HrQG-H8CxsvB0Dt-qKrrhHZEu6ETXSwbJWAq9UxvdOUktQQANdbWw=w408-h306-k-no'
+Kocoura = 'https://lh3.googleusercontent.com/gps-cs-s/AC9h4nohqUjmMj-1yoBftuhaAZN2AUGWZPwNPEWEUM2oIPBRcH5d6IzCQ18P0WcGN_KTj-tsyEBvhgiyTYGjdVQv-UkyEUvol9Tu1dFVwyvDkXGNS-9ic81mIgrZhgujfgOUUw=w408-h544-k-no'
+Hrocha = 'https://lh3.googleusercontent.com/gps-cs-s/AC9h4nq_z24ckyijhpok42nf1p5Xq4TXY7XlU9-XnWAE39T66H3IyET5WASQRQuqayT1Zwv0VMSJYkGN60VqU-yG_ins6xfO1OygAhbAwt4SULfLdOBL5iBMDBwmn27dk77R01uWx92r=w408-h240-k-no-pi-20-ya357.3678-ro-0-fo100'
+Mål = 'https://lh3.googleusercontent.com/gps-cs-s/AC9h4nr6IZwKcfVQoZcS7QUglsZdwZ1hoa-cuUFg7TdEtilWubxmAxflfPvDX_vAl8psxzo3UQIC-WIguznk4HHREWPPF_kboP_VXpDGjFx9r-W_EPU0ZigDHXbW1SFeXjwSKQVSACMp=w408-h305-k-no'
+
+
---
+++ main.py
@@ -0,0 +1,65 @@
+import inputdata
+import folium
+from folium.features import DivIcon
+import pandas as pd
+import webbrowser
+
+
+def main():
+    start = 50.086, 14.414
+
+    data = pd.DataFrame({
+        "lat": inputdata.latitude,
+        "lon": inputdata.longitude,
+        "name": inputdata.pubname,
+        "addr": inputdata.pubaddress,
+        "img": [inputdata.Rokoko, inputdata.Pinkasů, inputdata.Irish, inputdata.Tygra, inputdata.Márnice,
+                inputdata.Kocoura, inputdata.Hrocha, inputdata.Mål]
+    })
+
+    m = folium.Map(location=start, width=700, height=500, zoom_start=15, min_zoom=5, max_zoom=18)
+
+    for i in range(0, len(data)):
+        folium.Marker(
+            location=[data.iloc[i]["lat"], data.iloc[i]["lon"]],
+            popup=data.iloc[i]['addr'],
+            icon=DivIcon(html=f"""<div style="font-size: 12pt;font-family: helvetica;
+             color: black">{data.iloc[i]["name"]}</div>""")).add_to(m)
+
+    tooltip = "Klikk på meg"
+
+    for i in range(0, len(data)):
+        html = f"""<div>
+            <img src={data.iloc[i]["img"]}>
+            <br /><span>{data.iloc[i]["name"]}</span>
+            <br /><span>{data.iloc[i]["addr"]}</span>
+            </div>"""
+        iframe = folium.IFrame(html)
+        popup = folium.Popup(iframe, min_width=500, max_width=700)
+        folium.Marker(
+            location=[data.iloc[i]["lat"], data.iloc[i]["lon"]],
+            popup=popup,
+            icon=folium.Icon(icon="beer", prefix="fa", color="blue"),
+            tooltip=tooltip,
+        ).add_to(m)
+
+    title = "Praha 2025"
+    title_html = """
+                 <h3 align="center" style="width:40%;"font-size:22px"><b>{}</b></h3>
+                 """.format(title)
+    m.get_root().html.add_child(folium.Element(title_html))
+
+    # folium.TileLayer('OpenStreetMap', attr='folium.TileLayer').add_to(m)
+    # folium.TileLayer('Stamen Toner', attr='folium.TileLayer').add_to(m)
+    # folium.TileLayer('Stamen Water Color', attr='folium.TileLayer').add_to(m)
+    # folium.TileLayer('cartodbpositron', attr='folium.TileLayer').add_to(m)
+    # folium.TileLayer('cartodbdark_matter', attr='folium.TileLayer').add_to(m)
+    # folium.LayerControl().add_to(m)
+
+    m.save("index.html")
+
+    webbrowser.open("index.html")
+
+
+if __name__ == '__main__':
+    main()
