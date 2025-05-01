e987812583fca2e06830ab91170cef92ebe3ad12
jefugl <107198804+jefugl@users.noreply.github.com>
2025-05-01 18:50:44 +0200

    first try

---
+++ gammel_inputdata.py
@@ -0,0 +1,12 @@
+latitude = [50.08185541084051, 50.083354712955504, 50.086042340227515, 50.08365749139562, 50.07935907165436,
+            50.08935502601786, 50.089202829502184, 50.09181099398295, ]
+longitude = [14.426490665960578, 14.423332896648573, 14.41804056781281, 14.418907324110208, 14.4169441617276,
+             14.403182681305024, 14.401563025484922, 14.401444532099037, ]
+
+pubname = ["1.EA Hotel Rokoko", "2.Pinkasů", "3.Zlatého Tygra", "4.Medvídků", "5.Fleků", "6.Hrocha",
+           "7.Krále Brabantského", "8.Borgen", ]
+pubaddress = ["Henrik Ibsens gate 90, 0255 Oslo", "Skovveien 3, 0257 Oslo", "Frognerveien 12B, 0263 Oslo",
+              "Skovveien 15, 0257 Oslo", "Frognerveien 30, 0263 Oslo", "Niels Juels gate 31, 0257 Oslo",
+              "Behrens gate 2, 0257 Oslo", "Bygdøy allé 59, 0265 Oslo", ]
+
+
